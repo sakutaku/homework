1d36b531cc732977f487205aa54f6e43c5d8af49
Abytova Saikal <saikalabytova1@gmail.com>
2023-05-11 18:05:42 +0600

    Added task-2

--- board.ts
+++ board.ts
@@ -1,1 +1,15 @@
+let board = '';
 
+for (let i = 1; i <= 8; i++) {
+    board = board + '\n';
+
+    for (let j = 1; j <= 8; j++) {
+        if (j % 2 === i % 2) {
+            board = board + '  ';
+        } else {
+            board = board + ' ██';
+        }
+    }
+}
+
+console.log(board);
